d35d8caf786249bbe47da16daf071e97aa315bda
Elis Mattosinho <99125499+eliscmattosinho@users.noreply.github.com>
2025-12-23 19:20:03 -0300

    refactor(footer): reorganize structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,35 @@ import "./Footer.css";
 
 function Footer() {
     return (
-        <div id="footer-block">
+        <footer id="footer-container">
             <div id="footer-content">
                 <div className="footer-container">
-                    <p className="p-footer">Todos os direitos reservados.</p>
-                    <p className="p-footer">2025</p>
+                    <p className="footer-text">
+                        Este projeto foi idealizado a partir da versão Notion:{' '}
+                        <a
+                            href="https://eliscmattosinho.notion.site/Ecossistema-de-leitura-digital-15432edc5fc5805a8ecfe3447f2d3d0b"
+                            className="notion-link"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Ecossistema de Leitura Digital &#40;2025&#41;
+                        </a>
+                    </p>
                 </div>
 
-                <a id="p-state" className="btn-p-state" href="https://eliscmattosinho.github.io/development-board/" target="_blank" rel="noopener noreferrer">
-                    Board de desenvolvimento
+                <p className="footer-text">Próximo projeto:</p>
+
+                <a
+                    id="btn-project"
+                    className="btn"
+                    href="https://eliscmattosinho.github.io/development-hub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <span>Development Hub</span>
                 </a>
             </div>
-        </div>
+        </footer>
     );
 }
 
