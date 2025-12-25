3cbb5937e5d4394d7bf45ec4ede7e56160fb284e
Elis Mattosinho <99125499+eliscmattosinho@users.noreply.github.com>
2025-12-24 21:29:03 -0300

    refactor(epicframes): initial codebase refactoring

--- src/components/Epic/components/EpicFrames/EpicFrames.jsx
+++ src/components/Epic/components/EpicFrames/EpicFrames.jsx
@@ -1,48 +1,69 @@
 import { MdLockOutline } from "react-icons/md";
+import { useHoldSwipeUnlock } from "./useHoldSwipeUnlock";
+import { useScreen } from "@/context/ScreenContext";
 
-import "./EpicFrames.css"
+import "./EpicFrames.css";
 
 export default function EpicFrames({
     epics,
     theme,
     clicked,
     selectedEpic,
-    onSelect
+    onSelect,
 }) {
-    return epics.map((epic, index) => (
-        <div
-            key={index}
-            id={epic.identificador}
-            className={`mockup-frame ${
-                clicked && selectedEpic !== epic.identificador ? "hide" : ""
-            }`}
-        >
-            <div className="frame-container">
-                <img
-                    className={`img-frame frame-${theme}`}
-                    src={epic.image}
-                    alt={epic.title}
-                />
+    const { isTouch } = useScreen();
 
-                <span className={`hide-epic hide-${theme}-epic`} />
-                <span className="cam-point" />
+    return epics.map((epic, index) => {
+        const { btnRef, bind } = useHoldSwipeUnlock({
+            isTouch,
+            onUnlock: () =>
+                onSelect(
+                    epic.identificador,
+                    epic.title || epic.titulo_epico
+                ),
+        });
 
-                <div className="frame-action">
-                    <h4 className="frame-title">{epic.title || epic.titulo_epico}</h4>
-                    <MdLockOutline />
-                    <button
-                        className="btn btn-unlock"
-                        onClick={() =>
-                            onSelect(
-                                epic.identificador,
-                                epic.title || epic.titulo_epico
-                            )
-                        }
-                    >
-                        Desbloquear
-                    </button>
+        return (
+            <div
+                key={index}
+                id={epic.identificador}
+                className={`mockup-frame ${clicked && selectedEpic !== epic.identificador
+                    ? "hide"
+                    : ""
+                    }`}
+            >
+                <div className="frame-container">
+                    <img
+                        className={`img-frame frame-${theme}`}
+                        src={epic.image}
+                        alt={epic.title}
+                    />
+
+                    <span className={`epic-overlay hide-${theme}-epic`} />
+                    <span className="cam-point" />
+
+                    <h4 className="frame-title">
+                        {epic.title || epic.titulo_epico}
+                    </h4>
+
+                    <div className="frame-action">
+                        <button
+                            ref={btnRef}
+                            className="btn-unlock"
+                            type="button"
+                            {...bind}
+                        >
+                            <MdLockOutline size={20} />
+                        </button>
+
+                        <span className="swipe-hint">
+                            {isTouch
+                                ? "Hold & swipe up to unlock"
+                                : "Click to unlock"}
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    ));
+        );
+    });
 }
